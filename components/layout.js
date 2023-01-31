efb0b737f87ad543a96da5186fb09a6d1bffb919
Hyacinthe ABANDA <hyacinthabanda@gmail.com>
2023-01-31 13:46:47 +0100

    change title page

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Hyacinthe ABANDA'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'My first Next.js App'
 
 export default function Layout({ children, home }) {
   return (
